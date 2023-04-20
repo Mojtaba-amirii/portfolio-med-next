35797d6bef0209f1f97872d9c3f30aa228cafe8e
Mojtaba-amirii <Mojtaba.a.amiri@outlook.com>
2023-04-20 11:39:46 +0200

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,29 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Link from "next/link";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <nav>
+        <ul className="flex flex-row  first-letter:items-center justify-between font-bold m-4">
+          <li className="text-blue-700 hover:text-blue-400">
+            <Link href="/">Home</Link>
+          </li>
+          <li className="text-blue-700 hover:text-blue-400">
+            <Link href="/about">About Me</Link>
+          </li>
+          <li className="text-blue-700 hover:text-blue-400">
+            <Link href="/skills"> Skills</Link>
+          </li>
+          <li className="text-blue-700 hover:text-blue-400">
+            <Link href="/projects"> Projects</Link>
+          </li>
+          <li className="text-blue-700 hover:text-blue-400">
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <Component {...pageProps} />
+    </>
+  );
 }
---
+++ src/pages/about.js
@@ -0,0 +1,19 @@
+export default function About() {
+  return (
+    <div>
+      <h1>Mojtaba Amiri</h1>
+      <p>
+        This is my personal page.
+        <br />
+        Welcome to my corner of the Internet.
+        <br />
+        I have been working as a Software Developer for the past year.
+        <br />
+        My background is in fullstack developer.
+        <br />
+        I am looking for new opportunities.
+        <br />
+      </p>
+    </div>
+  );
+}
